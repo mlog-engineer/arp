c6920b64a74748c01c8b3d46dff8d82be5b3de3e
Clarmy Lee <clarmylee92510@gmail.com>
2018-12-18 11:36:11 +0800

    增加了avt7.com数据源

--- collecter.py
+++ collecter.py
@@ -14,8 +14,46 @@ import logging
 logger = logging.getLogger('root')
 
 
-def get_rpt_from_awc(icao,kind='metar'):
-    '''按机场ICAO码获取报文，数据来自 AWC: https://aviationweather.gov/
+def random_header():
+    '''随机获取请求Header信息'''
+    header_list = [
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv2.0.1)'\
+            ' Gecko/20100101 Firefox/4.0.1',
+        'Mozilla/5.0 (Windows NT 6.1; rv2.0.1) '\
+            'Gecko/20100101 Firefox/4.0.1',
+        'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) '\
+            'Presto/2.8.131 Version/11.11',
+        'Opera/9.80 (Windows NT 6.1; U; en) '\
+            'Presto/2.8.131 Version/11.11',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) '\
+            'AppleWebKit/535.11 (KHTML, like Gecko) '\
+            'Chrome/17.0.963.56 Safari/535.11',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) '\
+            'Gecko/20100101 Firefox/57.0'
+          ]
+    return random.choice(header_list)
+
+
+def get_web_code(url):
+    '''根据url获取网页源代码'''
+    req = urllib.request.Request(url)
+    header = random_header()
+    req.add_header('User-Agent',header)
+    try:
+        web_code = urllib.request.urlopen(req).read().decode('utf-8')
+    except KeyboardInterrupt:
+        exit()
+    except:
+        web_code = None
+    return web_code
+
+
+def get_single_rpt(icao,kind='metar',source='awc'):
+    '''按机场ICAO码获取单个机场指定类型的报文
+
+    数据来源
+    - 美国航空天气网(awc): https://aviationweather.gov/
+    - 小飞象(avt7): http://www.avt7.com/
 
     输入参数
     -------
@@ -23,6 +61,8 @@ def get_rpt_from_awc(icao,kind='metar'):
         所要查询的机场ICAO码，如'ZBAA'、'ZUUU'
     kind : `str`
         报文类型，须在'metar'和'taf'中选择，默认为'metar'
+    source : `str`
+        数据源，须在'awc'和'avt7'中选择，默认为'awc'
 
     返回值
     -----
@@ -30,79 +70,69 @@ def get_rpt_from_awc(icao,kind='metar'):
 
     示例
     ---
-    >>> from collecter import get_rpt_from_awc
-    >>> get_rpt_from_awc('ZBAA','metar')
+    >>> from collecter import get_single_rpt
+    >>> get_single_rpt('ZBAA','metar')
     'METAR ZBAA 030400Z 03005MPS 340V070 CAVOK 07/M21 Q1026 NOSIG'
     '''
-    def random_header():
-        header_list = [
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv2.0.1)'\
-                ' Gecko/20100101 Firefox/4.0.1',
-            'Mozilla/5.0 (Windows NT 6.1; rv2.0.1) '\
-                'Gecko/20100101 Firefox/4.0.1',
-            'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) '\
-                'Presto/2.8.131 Version/11.11',
-            'Opera/9.80 (Windows NT 6.1; U; en) '\
-                'Presto/2.8.131 Version/11.11',
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) '\
-                'AppleWebKit/535.11 (KHTML, like Gecko) '\
-                'Chrome/17.0.963.56 Safari/535.11',
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) '\
-                'Gecko/20100101 Firefox/57.0'
-              ]
-        return random.choice(header_list)
-
-    def get_url(icao,kind):
-        url = 'https://aviationweather.gov/adds/'\
-              'dataserver_current/httpparam?'\
-              'dataSource={0}s&requestType=retrieve&format=xml'\
-              '&stationString={1}&hoursBeforeNow=2'\
-              '&mostRecent=true'.format(kind,icao)
+    def get_url(icao,kind,source='awc'):
+        '''根据机场ICAO代号获取指定类型报文的URL'''
+        if source == 'awc':
+            url = 'https://aviationweather.gov/adds/'\
+                  'dataserver_current/httpparam?'\
+                  'dataSource={0}s&requestType=retrieve&format=xml'\
+                  '&stationString={1}&hoursBeforeNow=2'\
+                  '&mostRecent=true'.format(kind,icao)
+        elif source == 'avt7':
+            url = 'http://www.avt7.com/Home/'\
+            'AirportMetarInfo?airport4Code={0}'.format(icao)
         return url
 
-    def get_web_code(url):
-        req = urllib.request.Request(url)
-        header = random_header()
-        req.add_header('User-Agent',header)
-        try:
-            web_code = urllib.request.urlopen(req).read().decode('utf-8')
-        except KeyboardInterrupt:
-            exit()
-        except:
-            web_code = None
-        return web_code
 
-
-    def parse_rpt(web_code,kind='metar'):
-        metar_pattern = '[A-Z]{4} \d{6}Z [0-9A-Z\s/]+'
-        taf_pattern = 'TAF [A-Z]{4} \d{6}Z[0-9A-Z\s/]+'
-        if kind == 'metar':
-            try:
-                head_pattern = '<metar_type>[A-Z]+</metar_type>'
-                head_line = re.search(head_pattern,web_code).group()
-                head = re.search('[A-Z]+', head_line).group()
-                rpt = head+' '+re.search(metar_pattern,web_code).group()
-            except AttributeError:
-                rpt = None
-        elif kind == 'taf':
-            try:
-                rpt = re.search(taf_pattern,web_code).group()
-            except AttributeError:
-                rpt = None
+    def parse_rpt(web_code,kind='metar',source='awc'):
+        ''''''
+        if source == 'awc':
+            metar_pattern = '[A-Z]{4} \d{6}Z [0-9A-Z\s/]+'
+            taf_pattern = 'TAF [A-Z]{4} \d{6}Z[0-9A-Z\s/]+'
+            if kind == 'metar':
+                try:
+                    head_pattern = '<metar_type>[A-Z]+</metar_type>'
+                    head_line = re.search(head_pattern,web_code).group()
+                    head = re.search('[A-Z]+', head_line).group()
+                    rpt = head+' '+re.search(metar_pattern,web_code).group()
+                except AttributeError:
+                    rpt = None
+            elif kind == 'taf':
+                try:
+                    rpt = re.search(taf_pattern,web_code).group()
+                except AttributeError:
+                    rpt = None
+        elif source == 'avt7':
+            metar_pattern = '(METAR|SPECI).+?='
+            taf_pattern = 'TAF.+?='
+            if kind == 'metar':
+                try:
+                    rpt = re.search(metar_pattern,web_code).group()
+                except AttributeError:
+                    rpt = None
+            elif kind == 'taf':
+                try:
+                    rpt = re.search(taf_pattern,web_code).group()
+                except AttributeError:
+                    rpt = None
 
         return rpt
 
-    url = get_url(icao,kind)
+    url = get_url(icao,kind,source=source)
     web_code = get_web_code(url)
     if web_code:
-        rpt = parse_rpt(web_code,kind)
+        rpt = parse_rpt(web_code,kind,source=source)
     else:
         rpt = None
 
     return rpt
 
 
-def get_rpts(icaos,kind='metar'):
+def get_rpts(icaos,kind='metar',source='awc'):
     '''批量获取航空报文
 
     输入参数
@@ -117,7 +147,7 @@ def get_rpts(icaos,kind='metar'):
     rpts = {}
     total = len(icaos)
     for n,icao in enumerate(icaos):
-        rpt = get_rpt_from_awc(icao,kind=kind)
+        rpt = get_single_rpt(icao,kind=kind,source=source)
         if rpt:
             rpts[icao] = rpt
             print('{0}: ({2}/{3}) {1} finished'.format(datetime.utcnow(),
@@ -133,4 +163,4 @@ def get_rpts(icaos,kind='metar'):
 
 if __name__ == '__main__':
     icao, kind = sys.argv[1], sys.argv[2]
-    print(get_rpt_from_awc(icao,kind))
+    print(get_single_rpt(icao,kind,source='avt7'))
